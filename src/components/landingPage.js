fca970072c33cabcbfd49966111e5749bd096ee4
Molly Pribble <mollypribble1@mac.com>
2024-06-29 20:20:03 -0400

    preloading first images/loading bar w timeout

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CustomButton from './button';
 import vhs from '../media/vhs.png'
+import websitev3gif from '../media/websitev3.gif';
 
 class LandingPage extends React.Component {
 
@@ -8,8 +9,12 @@ class LandingPage extends React.Component {
         super(props);
         this.animateImg=this.animateImg.bind(this);
         this.animateAndNav=this.animateAndNav.bind(this);
+        this.preloadImages=this.preloadImages.bind(this);
+        this.preloadImage=this.preloadImage.bind(this);
+        this.delay=this.delay.bind(this);
         this.state = {
-            animate: false
+            animate: false,
+            loaded: false
         };
       }
 
@@ -22,14 +27,62 @@ class LandingPage extends React.Component {
         setTimeout(this.props.enterPortfolio, 1500);
       }
 
+      preloadImage = (src) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = src;
+          img.onload = resolve;
+          img.onerror = reject;
+          console.log("preload", img)
+        });
+      };
+
+      preloadImages = (imagesArray) => {
+        const promises = imagesArray.map((src) => this.preloadImage(src));
+        return Promise.all(promises);
+      };
+
+      delay(t) {
+        return new Promise(resolve => setTimeout(resolve, t));
+      }
+
+      componentDidMount() {
+        const loadImages = [
+          vhs,
+          websitev3gif
+        ]
+
+        this.preloadImages(loadImages).then(() => {
+          this.setState({ loaded: true });
+          console.log('all preloaded');
+        });
+
+        this.delay(6300).then(() => {
+          console.log("max timeout hit when loaded is: ", this.state.loaded);
+          this.setState({ loaded: true });
+        }
+        )
+      }
+
     render () {
         const animateClass = this.state.animate ? " animate" : ""
 
         return <>
-        <div className={'insert-vhs' + animateClass} />
-        <div className={'landing-page' + animateClass}>
-            <img className='landing-page-vhs-img' src={vhs} alt="Molly's UX portfolio now available on VHS. Play now!" width="80%" height="auto" />
-            <CustomButton small={false} bounce={true} icon={"play.png"} onClick={this.animateAndNav} />
+        <div className={this.state.loaded ? "hide-me" : "show-me"}>
+          <div className='loading-page'>
+            <div className='loading-bar'>
+              <div className='loading-bar-text'>
+              Loading...  Loading...  Loading...  Loading...  Loading...  Loading... 
+              </div> 
+            </div>
+          </div>
+        </div>
+        <div className={this.state.loaded ? "show-me" : "hide-me"} >
+          <div className={'insert-vhs' + animateClass} />
+          <div className={'landing-page' + animateClass}>
+              <img className='landing-page-vhs-img' src={vhs} alt="Molly's UX portfolio now available on VHS. Play now!" width="80%" height="auto" />
+              <CustomButton small={false} bounce={true} icon={"play.png"} onClick={this.animateAndNav} />
+          </div>
         </div>
         </>
     }
