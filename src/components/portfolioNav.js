5ae14288b0ea74e5eb3be0b1d1ada3cee7330bc9
Molly Pribble <mollypribble1@mac.com>
2024-09-12 07:03:55 -0400

    fix main link tab index

--- src/components/portfolioNav.js
+++ src/components/portfolioNav.js
@@ -59,7 +59,7 @@ class NavBar extends React.Component {
         const toggleLabel = (this.state.open) ? "Close navigation menu" : "Open navigation menu"
 
         return <nav className={navBarClass+first} aria-label={"Portfolio Navigation, " + label} >
-            <a href="#main" class="visually-hidden skip">Skip to main</a>
+            <a href="#main" class="visually-hidden skip" tabIndex={tabIndex}>Skip to main</a>
             <CustomButton disabled={false} className='nav-toggle' small={true} bounce={false} icon={icon} active={false} onClick={(e) => this.toggleOpen()} ariaLabel={toggleLabel} />
             <CustomButton disabled={disableButtons} tabIndex={tabIndex} small={false} bounce={false} icon={"home.png"} active={(this.props.currPage == 0)} onClick={(e) => this.handleNavWrapper(0)} ariaLabel="Go to home page" role="link" />
             <CustomButton disabled={disableButtons} tabIndex={tabIndex}  small={false} bounce={false} icon={"proj.png"} active={(this.props.currPage != 0 && this.props.currPage != 6)} onClick={(e) => this.handleNavWrapper(1)} ariaLabel="Go to projects list" role="link"/>
