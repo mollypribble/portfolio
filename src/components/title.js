5eb39c53abbac5b70989b720be3e9caa265dd754
Molly Pribble <mollypribble1@mac.com>
2024-09-12 07:40:55 -0400

    presentational stripes

--- src/components/title.js
+++ src/components/title.js
@@ -10,12 +10,12 @@ class Title extends React.Component {
 
         return <div className="project-title">
             <h1 className='text'>{this.props.title}</h1>
-            <div className='rect-1'></div>
-            <div className='rect-2'></div>
-            <div className='rect-3'></div>
-            <div className='rect-4'></div>
-            <div className='rect-5'></div>
-            <div className='rect-6'></div>
+            <div className='rect-1' role="presentation"></div>
+            <div className='rect-2' role="presentation"></div>
+            <div className='rect-3' role="presentation"></div>
+            <div className='rect-4' role="presentation"></div>
+            <div className='rect-5' role="presentation"></div>
+            <div className='rect-6' role="presentation"></div>
             </div>
     }
     
