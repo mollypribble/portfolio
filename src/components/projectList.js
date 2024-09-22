bf04390e6fb707b83307c84710544cafee84e242
Molly Pribble <mollypribble1@mac.com>
2024-09-22 11:14:25 -0400

    fix url bug

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -15,10 +15,10 @@ class ProjectList extends React.Component {
         return <div className="project-list-display">
                 <Title title="Projects"/>
                 <div className='project-list'>
-                    <ReactLink linkTo="/portfolio/home" child={<ProjectButton projName={"MindYoga"} projNumber={'1'} projDescription={"Design/HCI Research, Rapid Prototyping"} onClick={(e) => this.props.handleNewProj(2)}/>} />
-                    <ReactLink linkTo="/portfolio/home" child={<ProjectButton projName={"Active View Data"} projNumber={'2'} projDescription={"UX Design, Interaction Design, Prototyping, User Testing"} onClick={(e) => this.props.handleNewProj(3)}/>} />
-                    <ReactLink linkTo="/portfolio/home" child={<ProjectButton projName={"Recruit Right"} projNumber={'3'} projDescription={"Wireframing, User Personas"} onClick={(e) => this.props.handleNewProj(4)}/>} />
-                    <ReactLink linkTo="/portfolio/home" child={<ProjectButton projName={"Hold the Code"} projNumber={'4'} projDescription={"Communication Design, Project Management, Tech Ethics"} onClick={(e) => this.props.handleNewProj(5)}/>} />
+                    <ReactLink linkTo="/portfolio/projects/mindyoga" child={<ProjectButton projName={"MindYoga"} projNumber={'1'} projDescription={"Design/HCI Research, Rapid Prototyping"} onClick={(e) => this.props.handleNewProj(2)}/>} />
+                    <ReactLink linkTo="/portfolio/projects/active-view-data" child={<ProjectButton projName={"Active View Data"} projNumber={'2'} projDescription={"UX Design, Interaction Design, Prototyping, User Testing"} onClick={(e) => this.props.handleNewProj(3)}/>} />
+                    <ReactLink linkTo="/portfolio/projects/recruit-right" child={<ProjectButton projName={"Recruit Right"} projNumber={'3'} projDescription={"Wireframing, User Personas"} onClick={(e) => this.props.handleNewProj(4)}/>} />
+                    <ReactLink linkTo="/portfolio/projects/hold-the-code" child={<ProjectButton projName={"Hold the Code"} projNumber={'4'} projDescription={"Communication Design, Project Management, Tech Ethics"} onClick={(e) => this.props.handleNewProj(5)}/>} />
             </div>
         </div>
     }
