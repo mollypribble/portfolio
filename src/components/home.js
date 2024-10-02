4cdd81b607fe7d3adfd575e3ec107d17de272d56
Molly Pribble <mollypribble1@mac.com>
2024-10-02 16:42:02 -0400

    with home gltich effect

--- src/components/home.js
+++ src/components/home.js
@@ -53,6 +53,7 @@ class Home extends React.Component {
                     </div>
                     <h1 className='home-simple-name'>
                         Molly Pribble
+                        <div aria-hidden="true" className='glitch'><p>Molly Pribble</p></div>
                     </h1>
                     <h2 className='home-simple-title'>
                         UX designer
--- src/components/projectButton.js
+++ src/components/projectButton.js
@@ -13,7 +13,9 @@ class ProjectButton extends React.Component {
 
     render () {
         return <button role="link" className={"proj-button " + "button-" + this.props.projNumber} alt={this.props.alt} onClick={(e) => this.clickAndBlur(e)} aria-label={"Project " + this.props.projNumber + " " + this.props.projName}>
-            <div className='proj-label'>{this.props.projNumber}: {this.props.projName}</div>
+            <div className='proj-label'>
+                {this.props.projNumber}: {this.props.projName}
+            </div>
             <div className='proj-desc-arrow-wrapper'>
                 <div className='proj-desc'><p>{this.props.projDescription}</p></div>
                 <div className='proj-arrow' aria-hidden="true">&rarr;</div>
